31600365e1ee910f6720ddd201a6fed24c2c87d7
matthova <matthova87@gmail.com>
2020-01-12 09:25:11 -0800

    kinda working part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import { FaSadCry, FaAdobe } from 'react-icons/fa';
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SlideOutHorizontal = styled.div`
+  display: flex;
+  align-items: center;
+  label{
+    border: 4px solid black;
+    cursor: pointer;
+    position: absolute;
+    z-index: 99999;
+  }
+  input {
+    display: none;
+  }
+  .iconsWrapper {
+    position: absolute;
+    ul {
+      display: none;
+      list-style-type: none;
+    }
+    li{
+      padding: 0 5px;
+    }
+  }
+  input:checked ~ div.iconsWrapper {
+    ul {
+      display: flex;
+    }
+  }
+`;
+
+const MainLabel = styled.div`
+`;
+
+
+
+const SadCry = styled(FaSadCry)`
+`;
+
+const Adobe = styled(FaAdobe)`
+`
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SlideOutHorizontal>
+        <input type="checkbox" id="togglebox2" />
+        <label htmlFor="togglebox2">
+          <FaAdobe />
+        </label>
+        <div className="iconsWrapper">
+          <ul>
+            <li><SadCry /></li>
+            <li><SadCry /></li>
+            <li><SadCry /></li>
+            <li><SadCry /></li>
+          </ul>
+        </div>
+      </SlideOutHorizontal>
+    </Container>
   );
 }
 
